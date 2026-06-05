b499e9dc58461b6779a17b3390c56a5ae6622c0b
miguetoro16 <miguetoro16@gmail.com>
2026-06-04 20:38:21 -0500

    sse agregaron las variables que representan todos los indicadores con sus respectivas formulas, todas vistas en clase de fundamentos

--- SistemaFinanciero.java
+++ SistemaFinanciero.java
@@ -7,7 +7,7 @@ public class SistemaFinanciero {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== INGRESO DE DATOS FINANCIEROS ===");
+        System.out.println("=== INGRESO DE DATOS FINANCIEROS DE SU EMPRESA ===");
 
         System.out.print("Activo Corriente: ");
         double activoCorriente = sc.nextDouble();
@@ -47,6 +47,53 @@ public class SistemaFinanciero {
 
         System.out.print("Cuentas por pagar: ");
         double cxp = sc.nextDouble();
-        
+
+        /* -en esta parte se declararon las variables para realizar los calculos 
+        de los indicadores */
+
+        double utilidad = ingresos - costos - gastos;
+
+        //INDICADORES DE LIQUIDEZ
+        double capitalTrabajo = activoCorriente - pasivoCorriente;
+        double razonLiquidez = activoCorriente / pasivoCorriente;
+        double pruebaAcida = (activoCorriente - inventario) / pasivoCorriente;
+        double ratioCaja = efectivo / pasivoCorriente;
+
+        // INDICADORES DE ENDEUDAMIENTO
+        double endeudamientoTotal = pasivoTotal / activoTotal;
+        double concentracionCP = pasivoCorriente / pasivoTotal;
+        double endeudamientoFinanciero = pasivoTotal / patrimonio;
+        double apalancamiento = activoTotal / patrimonio;
+        double endeudamientoCP = pasivoCorriente / activoTotal;
+        double coberturaInteres = utilidad / intereses;
+        double ratioDeuda = pasivoTotal / activoTotal;
+        double deudaPatrimonio = pasivoTotal / patrimonio;
+
+        // INDICADORES DE RENTABILIDAD
+        double margenNeto = utilidad / ingresos;
+        double roa = utilidad / activoTotal;
+        double roe = utilidad / patrimonio;
+        double facturacionActivos = ingresos / activoTotal;
+        double rendimientoActivos = roa;
+        double rendimientoPatrimonio = roe;
+
+        // INDICADORES DE INVENTARIO
+        double rotacionInventario = costos / inventario;
+        double diasInventario = 365 / rotacionInventario;
+
+        // INDICADORES DE CREDITO
+        double volumenVentas = ingresos;
+        double diasVentasCredito = 365 / (ingresos / cxc);
+
+        // LA IDENTIDAD DUPONT
+        double dupont = margenNeto * facturacionActivos * apalancamiento;
+
+        // MEDIDAS DEL VALOR DEL MERCADO
+        double periodoInventario = diasInventario;
+        double periodoCobro = 365 / (ingresos / cxc);
+        double periodoPago = 365 / (costos / cxp);
+        double cicloOperativo = periodoInventario + periodoCobro;
+        double cicloCaja = cicloOperativo - periodoPago;
     }
+    
 }
