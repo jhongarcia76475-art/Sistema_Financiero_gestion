d9a1ade960048d94d933b9f4cbc6c273b1f025f3
jhon.garcia76475 <jhon.garcia76475@ucaldas.edu.co>
2026-06-08 17:42:14 -0500

    Se reemplazaron unos datos y se corrigio los datos mostrados

--- SistemaFinanciero.java
+++ SistemaFinanciero.java
@@ -17,8 +17,8 @@ public class SistemaFinanciero {
         String empresa = sc.nextLine(); 
 
         System.out.println(" =========================================================");
-        System.out.println("=== SEÑOR " + nombre + " SE REQUIEREN LOS DATOS DE SU EMPRESA ===");
-        System.out.println("=== INGRESO DE DATOS FINANCIEROS DE LA EMPRESA " + empresa + " EN EL ULTIMO AÑO FACTURADO ===");
+        System.out.println("=== SEÑOR " + nombre.toUpperCase() + " SE REQUIEREN LOS DATOS DE SU EMPRESA ===");
+        System.out.println("=== INGRESE LOS DATOS FINANCIEROS DE LA EMPRESA " + empresa.toUpperCase() + " EN EL ULTIMO AÑO FACTURADO ===");
 
         double activoCorriente = leerNumero(sc, "Activo Corriente: ");
         double inventario = leerNumero(sc, "Inventario: ");
@@ -35,6 +35,10 @@ public class SistemaFinanciero {
         double cxp = leerNumero(sc, "Cuentas por pagar: ");
         System.out.println(" =========================================================");
 
+
+        double utilidad = ingresos - costos - gastos;
+            int caso = 0;
+        do { 
         System.out.println(" === YA INGRESADOS LOS DATOS QUE DESEAS ENCONTRAR ===");
         System.out.println("1 == INDICADORES DE LIQUIDEZ");
         System.out.println("2 == INDICADORES DE ENDEUDAMIENTO");
@@ -44,10 +48,11 @@ public class SistemaFinanciero {
         System.out.println("6 == LA IDENTIDAD DUPONT");
         System.out.println("7 == MEDIDAS DEL VALOR DEL MERCADO");
         System.out.println("8 == VARIACION RELATIVA Y ABSOLUTA");
-        int caso = sc.nextInt();
+        System.out.println("0 == SALIR");
 
-        double utilidad = ingresos - costos - gastos;
-
+        caso = sc.nextInt();
+            
+        
         switch (caso) {
             case 1:
                 //INDICADORES DE LIQUIDEZ
@@ -160,153 +165,51 @@ public class SistemaFinanciero {
 
             case 8:
                 System.out.println("=PARA ESTA OPCION SE LE SOLICITA INGRESAR LOS DATOS FINANCIEROS DE LA EMPRESA " + empresa + " EN EL PENULTIMO AÑO FACTURADO =");
-                System.out.println(" =========================================================");
+                System.out.println(" ==================================================================================================================================");
 
-                int dato = 1;
-                System.out.println(dato);
                 double activoCorrienteDos = leerNumero(sc, "Activo Corriente: ");
-                variacion(activoCorriente,activoCorrienteDos,dato);
-                dato++;
-                System.out.println(dato);
                 double inventarioDos = leerNumero(sc, "Inventario: ");
-                variacion(inventario,inventarioDos,dato);
-                dato++;
-                System.out.println(dato);
                 double efectivoDos = leerNumero(sc, "Efectivo: ");
-                variacion(efectivo, efectivoDos, dato);
-                dato++;
-                System.out.println(dato);
                 double pasivoCorrienteDos = leerNumero(sc, "Pasivo Corriente: ");
-                variacion(pasivoCorriente,pasivoCorrienteDos,dato);
-                dato++;
-                System.out.println(dato);
                 double activoTotalDos = leerNumero(sc, "Activo Total: ");
-                variacion(activoTotal,activoTotalDos,dato);
-                dato++;
-                System.out.println(dato);
                 double pasivoTotalDos = leerNumero(sc, "Pasivo Total: ");
-                variacion(pasivoTotal,pasivoTotalDos,dato);
-                dato++;
-                System.out.println(dato);
                 double patrimonioDos = leerNumero(sc, "Patrimonio: ");
-                variacion(patrimonio,patrimonioDos,dato);
-                dato++;
-                System.out.println(dato);
                 double ingresosDos = leerNumero(sc, "Ingresos (Ventas): ");
-                variacion(ingresos,ingresosDos,dato);
-                dato++;
-                System.out.println(dato);
                 double costosDos = leerNumero(sc, "Costos: ");
-                variacion(costos,costosDos,dato);
-                dato++;
-                System.out.println(dato);
                 double gastosDos = leerNumero(sc, "Gastos: ");
-                variacion(gastos,gastosDos,dato);
-                dato++;
-                System.out.println(dato);
                 double interesesDos = leerNumero(sc, "Intereses: ");
-                variacion(intereses,interesesDos,dato);
-                dato++;
-                System.out.println(dato);
                 double cxcDos = leerNumero(sc, "Cuentas por cobrar: ");
-                variacion(cxc,cxcDos,dato);
-                dato++;
-                System.out.println(dato);
                 double cxpDos = leerNumero(sc, "Cuentas por pagar: ");
-                variacion(cxp,cxpDos,dato);
+
+                variacion("ACTIVO CORRIENTE",activoCorriente,activoCorrienteDos);
+                variacion("INVENTARIO",inventario,inventarioDos);
+                variacion("EFECTIVO",efectivo, efectivoDos);
+                variacion("PASIVO CORRIENTE",pasivoCorriente,pasivoCorrienteDos);
+                variacion("ACTIVO TOTAL",activoTotal,activoTotalDos);
+                variacion("PASIVO TOTAL",pasivoTotal,pasivoTotalDos);
+                variacion("PATRIMONIO",patrimonio,patrimonioDos);
+                variacion("INGRESOS",ingresos,ingresosDos);
+                variacion("COSTOS",costos,costosDos);
+                variacion("GASTOS",gastos,gastosDos);
+                variacion("INTERESES",intereses,interesesDos);
+                variacion("CUENTAS POR COBRAR",cxc,cxcDos);
+                variacion("CUENTAS POR PAGAR",cxp,cxpDos);
 
                 System.out.println(" =========================================================");
 
                 break;
+
+                case 0:
+                    System.out.println("HASTA PRONTO");
+
+                    break;
             default:
-                System.out.println("!!!!!!!!no elegiste una opcion valida¡¡¡¡¡¡¡¡¡¡");;
+                System.out.println("¡LA OPCION ES INVALIDA!");
         }
+        } while (caso !=0);
 
         
-        /* -en esta parte se declararon las variables para realizar los calculos 
-        de los indicadores 
-
         
-
-        //INDICADORES DE LIQUIDEZ
-        double capitalTrabajo = activoCorriente - pasivoCorriente;
-        double razonLiquidez = dividir(activoCorriente, pasivoCorriente, "Razón de liquidez");
-        double pruebaAcida = dividir((activoCorriente - inventario), pasivoCorriente, "Prueba ácida");
-        double ratioCaja = dividir(efectivo, pasivoCorriente, "Ratio de caja");
-
-        // INDICADORES DE ENDEUDAMIENTO
-        double endeudamientoTotal = dividir(pasivoTotal, activoTotal, "Endeudamiento total");
-        double concentracionCP = dividir(pasivoCorriente, pasivoTotal, "Concentración corto plazo");
-        double endeudamientoFinanciero = dividir(pasivoTotal, patrimonio, "Endeudamiento financiero");
-        double apalancamiento = dividir(activoTotal, patrimonio, "Apalancamiento");
-        double endeudamientoCP = dividir(pasivoCorriente, activoTotal, "Endeudamiento corto plazo");
-        double coberturaInteres = dividir(utilidad, intereses, "Cobertura de interés");
-        double ratioDeuda = dividir(pasivoTotal, activoTotal, "Ratio total deuda");
-        double deudaPatrimonio = dividir(pasivoTotal, patrimonio, "Deuda patrimonio");
-
-        // INDICADORES DE RENTABILIDAD
-        double margenNeto = dividir(utilidad, ingresos, "Margen neto");
-        double roa = dividir(utilidad, activoTotal, "ROA");
-        double roe = dividir(utilidad, patrimonio, "ROE");
-        double facturacionActivos = dividir(ingresos, activoTotal, "Facturación activos");
-
-        // INDICADORES DE INVENTARIO
-        double rotacionInventario = dividir(costos, inventario, "Rotación inventario");
-        double diasInventario = dividir(365, rotacionInventario, "Días inventario");
-
-        // INDICADORES DE CREDITO
-        double rotacionCartera = dividir(ingresos, cxc, "Rotación cartera");
-        double diasVentasCredito = dividir(365, rotacionCartera, "Días ventas crédito");
-
-        // LA IDENTIDAD DUPONT
-        double dupont = margenNeto * facturacionActivos * apalancamiento;
-
-        // MEDIDAS DEL VALOR DEL MERCADO
-        double periodoInventario = diasInventario;
-        double periodoCobro = diasVentasCredito;
-        double periodoPago = dividir(365, dividir(costos, cxp, "Rotación pagos"), "Periodo pago");
-        double cicloOperativo = periodoInventario + periodoCobro;
-        double cicloCaja = cicloOperativo - periodoPago;
-
-        // ================= RESULTADOS =================
-        System.out.println("\n===Indicadores===");
-
-        System.out.println("\n--- LIQUIDEZ ---");
-        mostrar("Capital de trabajo", capitalTrabajo);
-        mostrar("Razón de liquidez", razonLiquidez);
-        mostrar("Prueba ácida", pruebaAcida);
-        mostrar("Ratio de caja", ratioCaja);
-
-        System.out.println("\n--- ENDEUDAMIENTO ---");
-        mostrar("Endeudamiento total", endeudamientoTotal);
-        mostrar("Concentración corto plazo", concentracionCP);
-        mostrar("Endeudamiento financiero", endeudamientoFinanciero);
-        mostrar("Apalancamiento", apalancamiento);
-        mostrar("Endeudamiento corto plazo", endeudamientoCP);
-        mostrar("Cobertura de interés", coberturaInteres);
-        mostrar("Ratio total deuda", ratioDeuda);
-        mostrar("Deuda patrimonio", deudaPatrimonio);
-
-        System.out.println("\n--- RENTABILIDAD ---");
-        mostrar("Margen neto", margenNeto);
-        mostrar("ROA", roa);
-        mostrar("ROE", roe);
-        mostrar("Facturación activos", facturacionActivos);
-
-        System.out.println("\n--- ACTIVIDAD ---");
-        mostrar("Rotación inventario", rotacionInventario);
-        mostrar("Días inventario", diasInventario);
-        mostrar("Días ventas crédito", diasVentasCredito);
-
-        System.out.println("\n--- DUPONT ---");
-        mostrar("Identidad Dupont", dupont);
-
-        System.out.println("\n--- CICLOS ---");
-        mostrar("Periodo inventario", periodoInventario);
-        mostrar("Periodo cobro", periodoCobro);
-        mostrar("Periodo pago", periodoPago);
-        mostrar("Ciclo operativo", cicloOperativo);
-        mostrar("Ciclo de caja", cicloCaja);           */
     }
 
     //validacion para la division
@@ -338,14 +241,19 @@ public class SistemaFinanciero {
         if (Double.isNaN(valor)) {
             System.out.println(nombre + ": No calculable");
         } else {
-            System.out.println(nombre + ": " + valor);
+            System.out.println("=======================================================================");
+            System.out.println("| "+ nombre + ": " + valor+ " |");
+            System.out.println("=======================================================================");
         }
     }
-    public static void variacion(double añoDos, double añoUno, int dato ){
+    public static void variacion(String mensaje, double añoDos, double añoUno){
         double variacionAbsoluta = añoDos - añoUno;
-        System.out.println("la variacion absoluta para el dato " + dato + " es de " + variacionAbsoluta);
         double variacionRelativa = variacionAbsoluta / añoUno; 
-        System.out.println("la variacion relativa para el dato " + dato + " es de " + variacionRelativa);
+        System.out.println("=======================================================================================================");
+        System.out.print("| " + mensaje + " | la variacion absoluta: " + variacionAbsoluta);
+        System.out.println(" | la variacion relativa: " + variacionRelativa + " |");
+        System.out.println("=======================================================================================================");
+        
     }
     
 }
